506de497f32f8dd24476db1960421b5db180d4b6
Gaya Kessler <gaya.kessler@gmail.com>
2014-07-29 15:56:18 +0200

    Added Bootstrap

--- markpress/classes/MarkPress.php
+++ markpress/classes/MarkPress.php
@@ -7,6 +7,7 @@ class MarkPress {
 
 		$this->killTheTheme();
 		$this->loadPageDown();
+		$this->loadBootstrap();
 		$this->loadAssets();
 	}
 
@@ -35,6 +36,11 @@ class MarkPress {
 
 	function loadPageDown() {
 		wp_enqueue_script("PageDown", plugins_url('markpress/assets/js/Markdown.Converter.js', $this->plugin_dir), array("jquery"), 2.2, false );
+	}
+
+	function loadBootstrap() {
+		wp_enqueue_script("Bootstrap", plugins_url('markpress/assets/bootstrap/js/bootstrap.min.js', $this->plugin_dir), array(), 3.2, false );
+		wp_enqueue_style("Bootstrap", plugins_url('markpress/assets/bootstrap/css/bootstrap.min.css', $this->plugin_dir), array(), 3.2);
 	}
 
 	function loadAssets() {
