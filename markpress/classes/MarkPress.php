340b504925cf0890677b8f888071fb4317012f17
Gaya Kessler <gaya.kessler@gmail.com>
2014-07-29 14:57:49 +0200

    Killed the theme

--- markpress/classes/MarkPress.php
+++ markpress/classes/MarkPress.php
@@ -5,11 +5,23 @@ class MarkPress {
 	function __construct($plugin_dir) {
 		$this->plugin_dir = $plugin_dir;
 
-		add_filter('template_include', array($this, 'killTheme'), 99);
+		$this->killTheTheme();
 	}
 
-	function killTheme($template) {
-		$template = $this->plugin_dir . "/template/yo.php";
+	function killTheTheme() {
+		add_filter('template_include', array($this, 'catchThemeInclude'), 99);
+		add_filter('template_directory', array($this, 'catchTemplateFolder'), 99);
+		add_filter('stylesheet_directory', array($this, 'catchTemplateFolder'), 99);
+	}
+
+	function catchTemplateFolder() {
+		$template_dir = $this->plugin_dir . "template";
+
+		return $template_dir;
+	}
+
+	function catchThemeInclude() {
+		$template = $this->plugin_dir . "template/index.php";
 
 		return $template;
 	}
---
+++ markpress/template/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		Hier komt de content
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
