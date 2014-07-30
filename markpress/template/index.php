8c95dd393d334af297f386876b8de6e16e77820f
Gaya Kessler <gaya.kessler@gmail.com>
2014-07-30 16:22:15 +0200

    Responsi

--- markpress/template/index.php
+++ markpress/template/index.php
@@ -1,16 +1,22 @@
-<?php
-get_header(); ?>
+<?php get_header(); ?>
 
-	<aside id="entries">
-		<?php get_template_part('aside', 'side-bar'); ?>
-	</aside>
-
-	<div id="primary" class="site-content">
-		<?php get_template_part('form', 'input-form'); ?>
-
-		<section class="markpress-editor__preview">
-			<div id="markpress-preview"></div>
+	<section id="markpress">
+		<section id="actions">
+			<button class="entries">entries</button>
+			<button class="mode">view mode</button>
+			<button class="save">save</button>
 		</section>
-	</div><!-- #primary -->
 
+		<aside id="entries">
+			<?php get_template_part('aside', 'side-bar'); ?>
+		</aside>
+
+		<div id="primary" class="site-content">
+			<?php get_template_part('form', 'input-form'); ?>
+
+			<section class="markpress-editor__preview">
+				<div id="markpress-preview"></div>
+			</section>
+		</div>
+	</section>
 <?php get_footer(); ?>
