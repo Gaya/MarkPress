b1bd28a60bdf6e110b12872bd7c9c7fe28089d84
Gaya Kessler <gaya.kessler@gmail.com>
2014-08-05 16:24:47 +0200

    disable zoom on mobile

--- markpress/template/header.php
+++ markpress/template/header.php
@@ -10,7 +10,7 @@
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
-	<meta name="viewport" content="width=device-width"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 	<title><?php wp_title(); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11"/>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
